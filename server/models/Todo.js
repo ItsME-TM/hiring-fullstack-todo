e6eae90150c201ba2a1a008b5836fd1e5ba674ff
ItsME-TM <thushanmalaka123@gmail.com>
2025-11-09 11:54:46 +0530

    Created the Todo model and export it

---
+++ server/models/Todo.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const todoSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+
+        },
+        description: {
+            type: String,
+            trim: true,
+        },
+        done: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+export default Todo;

de9af1337420858fa673f72b01ddba49d4f16f5b
ItsME-TM <thushanmalaka123@gmail.com>
2025-11-10 15:35:19 +0530

    Implemeted the TodoList component

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,11 +1,40 @@
 //Shows the list of todos
+import TodoItem from "./TodoItem";
+TodoList = [
+    {
+        _id: "1",
+        title: "Sample Todo 1",
+        description: "This is a sample description for todo 1.",
+        done: false,
+    },
+    {
+        _id: "2",
+        title: "Sample Todo 2",
+        description: "This is a sample description for todo 2.",
+        done: true,
+    }
 
+]
 
-const API_URL = import.meta.env.VITE_API_URL;
+const onUpdate = (updatedTodo) => {
+    console.log("Update Todo:", updatedTodo);
+}
+
+const onDelete = (todoId) => {
+    console.log("Delete Todo with ID:", todoId);
+}
+
 export default function TodoList() {
     return(
-        <div>
-            Todo List Component
-        </div>
+        <ul>
+            {TodoList.map((todo) => (
+                <TodoItem
+                    key = {todo._id}
+                    todo = {todo}
+                    onUpdate = {onUpdate}
+                    onDelete = {onDelete}
+                />
+            ))}
+        </ul>
     );
 }
